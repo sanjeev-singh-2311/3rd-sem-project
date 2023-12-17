6f2b3f8f0b6c5d3efd9e46d122a8da5b2b1aef4e
sanjeev-singh-2311 <sanjeev.2311.singh@gmail.com>
2023-12-17 21:37:47 +0530

    added allowed hosts

--- ProjectBackend/thirdsemproj/settings.py
+++ ProjectBackend/thirdsemproj/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-y&om&n4hoshv_vm77kls2z6fu@7^i+a%2qn3lvl&vyz!mwiry_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
