6ed037c59b29ce84f91549d8ab5b14fdf9f33a4e
sanjeev-singh-2311 <sanjeev.2311.singh@gmail.com>
2023-12-18 11:09:19 +0530

    Made database for user authentication

---
+++ ProjectBackend/UserAuth/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+# Create your models here.
+class UserData(models.Model):
+
+	username = models.CharField(max_length=50, verbose_name = "UserName")
+	email = models.EmailField(max_length=254, verbose_name = "UserEmail")
+	password = models.CharField( max_length=50, verbose_name = "UserPass")
+
+	class Meta:
+		verbose_name = "UserData"
+		verbose_name_plural = "UserDatas"
+
+	def __str__(self):
+		return self.name
+
+	def get_absolute_url(self):
+		return reverse("UserData_detail", kwargs={"pk": self.pk})
